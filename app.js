3e37704a58865acf7d65e882786be9dff5bb6302
Robert Abreu <robertabreu@pursuit.org>
2019-05-01 21:25:06 -0400

    created restaurants router and exported to app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
 const app = require('express')();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { RestaurantRouter } = require('./routes/');
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use('/restaurants', RestaurantRouter);
 
 app.use((req, res) => {
     res.json({
---
+++ routes/restaurants.js
@@ -0,0 +1,44 @@
+const RestaurantRouter = require('express').Router();
+const {
+    getByCoord,
+    getByAddress
+} = require('../services/main');
+
+RestaurantRouter.get('/location/', (req, res) => {
+    const {
+        address
+    } = req.query;
+    getByAddress(address)
+        .then(parsedData => {
+            res.json({
+                restaurants: parsedData
+            }).status(200);
+        })
+        .catch(_ => {
+            res.json({
+                message: 'Error fetching restaurant data'
+            }).status(200);
+        });
+        return;
+});
+
+RestaurantRouter.get('/geo/', (req, res) => {
+    const {
+        lat,
+        lon
+    } = req.query;
+    getByCoord(lat, lon)
+        .then(parsedData => {
+            res.json({
+                restaurants: parsedData
+            }).status(200);
+        })
+        .catch(_ => {
+            res.json({
+                message: 'Error fetching restaurant data'
+            }).status(200);
+        });
+        return;
+});
+
+module.exports = RestaurantRouter;
